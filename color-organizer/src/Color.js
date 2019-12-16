775e28fe0b78f33b8e1f92b9b06d517b5e7b760b
Alberto Silva <silvaguzmanalberto@gmail.com>
2019-12-16 14:31:56 -0600

    Allow remove colors and reflect on App state

--- color-organizer/src/Color.js
+++ color-organizer/src/Color.js
@@ -1,10 +1,17 @@
 import React from 'react';
+
+import IconButton from '@material-ui/core/IconButton';
+import { FaTrash } from "react-icons/fa";
+
 import StarRating from './StarRating';
 
-export default function Color({ title, color, rating }) {
+export default function Color({ id, title, color, rating, onRemove = f => f }) {
     return (
       <section>
         <h1>{title}</h1>
+        <IconButton aria-label="delete" onClick={() => onRemove(id)}>
+          <FaTrash color="red" />
+        </IconButton>
         <div style={{ height: 50, backgroundColor: color }} />
         <StarRating selectedStars={rating} />
       </section>
--- color-organizer/src/ColorList.js
+++ color-organizer/src/ColorList.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Color from "./Color";
 
-export default function ColorList({ colors = [] }) {
+export default function ColorList({ colors = [], onRemoveColor = f => f  }) {
   return (
     <div>
       {colors.length === 0 ? (
         <p>No Colors Listed. (Add a Color)</p>
       ) : (
-        colors.map(color => <Color key={color.id} {...color} />)
+        colors.map(color => <Color key={color.id} {...color} onRemove={onRemoveColor}/>)
       )}
     </div>
   );
